0c166894f596acf83399c4f086c3b12d1e2e5560
Laurie Denness <laurie@denness.net>
2011-04-21 11:19:22 +0100

    Use ImageView widget for the images. Now zoomable, all images viewable, swipe left and right to change image, pinch to zoom

--- app/assistants/listingView-assistant.js
+++ app/assistants/listingView-assistant.js
@@ -1,3 +1,11 @@
+curPhotoIndex = 1;
+positionDelta = {
+	left: -1,
+	center: 0,
+	right: 1
+};
+imagesarray = [];
+
 function ListingViewAssistant(tappeditem) {
 	/* this is the creator function for your scene assistant object. It will be passed all the 
 	   additional parameters (after the scene name) that were passed to pushScene. The reference
@@ -14,9 +22,54 @@ ListingViewAssistant.prototype.setup = function() {
 	/* use Mojo.View.render to render view templates and add them to the scene, if needed */
 	
 	/* setup widgets here */
+	this.leftHandler = this.leftHandler.bind(this);
+	this.rightHandler = this.rightHandler.bind(this);	
+
+	this.controller.setupWidget('listingImage', 
+		this.attributes = {
+			noExtractFS: true
+		}, 
+		this.viewerModel = { 
+			onLeftFunction: this.leftHandler,
+			onRightFunction: this.rightHandler
+		}
+	);
 	
 	/* add event handlers to listen to events from widgets */
 };
+
+ListingViewAssistant.prototype.leftHandler = function() {
+		this.movePhotoIndex('left');
+		this.controller.get("listingImage").mojo.leftUrlProvided(this.getUrlForThe('left'));
+}
+
+ListingViewAssistant.prototype.rightHandler = function() {
+		this.movePhotoIndex('right');
+		this.controller.get("listingImage").mojo.rightUrlProvided(this.getUrlForThe('right'));
+}
+
+
+ListingViewAssistant.prototype.movePhotoIndex = function (direction) {
+           	curPhotoIndex = curPhotoIndex + positionDelta[direction];
+                
+                if(curPhotoIndex > imagesarray.length-1 || curPhotoIndex < 1) {        
+                        curPhotoIndex = this.wrapAroundMarioStyle( curPhotoIndex, imagesarray.length );
+                }
+};
+                
+ListingViewAssistant.prototype.getUrlForThe = function( position ){
+                var urlIndex;
+                urlIndex = curPhotoIndex + positionDelta[position];
+                
+                if(urlIndex > imagesarray.length-1 || urlIndex < 0) {    
+                        urlIndex = this.wrapAroundMarioStyle( urlIndex, imagesarray.length ); 
+                }
+                        
+                return imagesarray[urlIndex];
+        };
+ListingViewAssistant.prototype.wrapAroundMarioStyle = function( index, max ){
+                return Math.abs( Math.abs( index ) - max );
+        };
 
 ListingViewAssistant.prototype.activate = function(event) {
 	/* put in event handlers here that should only be in effect when this scene is active. For
@@ -34,17 +87,19 @@ ListingViewAssistant.prototype.cleanup = function(event) {
 };
 
 ListingViewAssistant.prototype.getImageURLs = function(listingid) {
-// TODO: Let's make this a ImageViewer! Those sound fun. 
 	var url = apibase + 'public/listings/' +listingid + '/images?api_key=' + apikey;
 	var request = new Ajax.Request(url, {
 		method: 'get',
 		onSuccess: function(transport){
 			var data = transport.responseText.evalJSON();
 			if (data.results != null) {
-				this.listingimageurl = data.results[0].url_170x135;
-				this.imagesrc = '<img src="' + this.listingimageurl + '">';
+				for (var i = 0; i<data.results.length; i++) {
+					imagesarray[i] = data.results[i].url_570xN;
+				}
 			}
-			this.controller.get("listingImage").update(this.imagesrc);
+			this.controller.get("listingImage").mojo.centerUrlProvided(this.getUrlForThe('center'));
+			this.controller.get("listingImage").mojo.leftUrlProvided(this.getUrlForThe('left'));
+			this.controller.get("listingImage").mojo.rightUrlProvided(this.getUrlForThe('right'));
 		}.bind(this),
 		onFailure: function(){ 
 			Mojo.Log.error('Failed to get response from Etsy API');
